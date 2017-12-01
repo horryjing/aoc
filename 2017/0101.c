521acec9d05a08d8d41111e6500ed6e856f1f452
horry_jing <horry_jing@aliyun.com>
2017-12-01 22:31:59 +0100

    the first part of day one

--- 2017/0101.c
+++ 2017/0101.c
@@ -1,11 +1,27 @@
 #include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 
-#define DATA_LEN 64
+#define DATA_LEN 4096
+
+unsigned int c2ui(char input)
+{
+  unsigned int ret = 0;
+  ret = input - '0';
+  return ret;
+}
 
 int main(int argc, char **argv)
 {
   FILE *fp;
   char data[DATA_LEN];
+  char first = 0;
+  char last = 0;
+  unsigned int len = 0;
+  unsigned int i = 0;
+  unsigned int c = 0;
+  unsigned int n = 0;
+  unsigned int total = 0;
 
   if (argc != 2)
   {
@@ -22,8 +38,28 @@ int main(int argc, char **argv)
   while (fgets(data, DATA_LEN, fp) != NULL)
   {
     printf("%s", data);
-    
   }
+  printf("\n");
+
+  len = strlen(data) - 1;
+  first = c2ui(data[0]);
+  last = c2ui(data[len-1]);
+  for (i=0; i<len-1; i++)
+  {
+    c = c2ui(data[i]);	
+    n = c2ui(data[i+1]);
+    if (c == n)
+	{
+	  total += c;
+	}
+  }
+
+  if (last == first)
+  {
+    total += last;
+  }
+  
+  printf("total %d\n", total);
 
   fclose(fp);
   return 0;
